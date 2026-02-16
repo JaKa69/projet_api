ff22c3f7f3484270481082a35603a5179428bf91
alix babin <alix.babin69@gmail.com>
2026-02-16 10:28:56 +0100

    test

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,19 +1,39 @@
-const authService = require("../services/auth.service.js");
+const userAuthService = require("../services/auth.service");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
-module.exports.register = async (req, res) => {
-  try {
-    const user = await authService.register(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
+module.exports.login = async(req, res) =>{
+    try {
+        const { username, password} = req.body;
+        let user = await userAuthService.getUser({ username:username});
+        if(!user) {
+            return res.status(401).json({ error:'Authentication failed'});
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if(!passwordMatch) {
+            return res.status(401).json({ error:'Authentication failed'});
+        }
+        // crée et retourne le token
+        const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRATION
+        });
+        return res.status(200).json({ token});
+    } catch(e) {
+        return res.status(400).json({status:400, message: e.message});
+    }
+}
 
-module.exports.login = async (req, res) => {
-  try {
-    const result = await authService.login(req.body.email, req.body.password);
-    res.json(result);
-  } catch (err) {
-    res.status(401).json({ message: err.message });
-  }
-};
+module.exports.register = async(req, res) =>{
+    try{
+        // crée un user d'authentification
+        let userAuth = User(req.body);
+        // hash le mdp avec bcrypt
+        let salt = await bcrypt.genSalt(10);
+        userAuth.password = await bcrypt.hash(userAuth.password, salt);
+        userAuth = await userAuthService.createUser(userAuth);
+        return res.status(201).json({status:201, data: userAuth, message:"Succesfully User auth Created"});
+    } catch(e) {
+        return res.status(400).json({status:400, message: e.message});
+    }
+}
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,28 +1,17 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const User = require("../models/User.js");
 
-module.exports.register = async (data) => {
-  const hashedPassword = await bcrypt.hash(data.password, 10);
-
-  return User.create({
-    ...data,
-    password: hashedPassword
-  });
-};
-
-module.exports.login = async (email, password) => {
-  const user = await User.findOne({ email });
-  if (!user) throw new Error("Utilisateur introuvable");
-
-  const isValid = await bcrypt.compare(password, user.password);
-  if (!isValid) throw new Error("Mot de passe incorrect");
-
-  const token = jwt.sign(
-    { id: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" }
-  );
-
-  return { user, token };
-};
+// récupère un user
+module.exports.getUser = async(query) =>{
+    try{
+        let user = await User.findOne(query);
+        return user;
+    }catch(e) {
+        throwError("Error while query all one user : " + e);
+}}
+// crée un user
+module.exports.createUser = async(user) =>{
+    try {
+        return await User.save();
+    } catch(e) {
+        throwError("Error while create user : "+e);
+}}
